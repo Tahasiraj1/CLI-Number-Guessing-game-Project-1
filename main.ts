7170357042d935b6305893181dd3445ea1611fab
Tahasiraj1 <160783918+Tahasiraj1@users.noreply.github.com>
2024-06-30 10:56:03 +0500

    Add files via upload
    This guess the number game is a short TypeScript/Node.js project that allows the user to guess the number generated by the computer. There are also several ways to alter the game, like adding more rounds or displaying the score. It’s quite simple and uses the random function to generate a number.

---
+++ main.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer"
+
+const RandomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNum",
+        type: 'number',
+        message: "Guess a number between 1-10: "
+    }
+]);
+
+if (answers.userGuessedNum === RandomNumber){
+    console.log("Congragulations, You've guessed the right number.")
+} else {
+    console.log("Try again!")
+}
